ee1d504b6eb20aeda855a7ab560ee9f835bf4f7b
Juandmunoz0990 <juand.munozm@uqvirtual.edu.co>
2025-11-11 11:29:24 -0500

    Especificaciones mejoradas

--- src/main/java/co/edu/uniquindio/application/mappers/HousingMapper.java
+++ src/main/java/co/edu/uniquindio/application/mappers/HousingMapper.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.application.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import co.edu.uniquindio.application.Dtos.Housing.Requests.CreateOrEditHousingRequest;
 import co.edu.uniquindio.application.Dtos.Housing.Responses.HousingResponse;
@@ -10,7 +11,9 @@ import co.edu.uniquindio.application.Models.Housing;
 @Mapper(componentModel = "spring")
 public interface HousingMapper {
     
+    @Mapping(source = "pricePerNight", target = "nightPrice")
     Housing toHousing(CreateOrEditHousingRequest request);
+    
     SummaryHousingResponse toSummaryHousingResponse (Housing housing);
     HousingResponse toHousingResponse (Housing housing);
 }
